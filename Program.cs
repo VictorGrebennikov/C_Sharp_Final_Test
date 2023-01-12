f33ffab3dd4b6646f821de4e439bb03d8af77a20
Victor Grebennikov <victorgrebennikov4@gmail.com>
2023-01-12 23:35:00 +0700

    programm code added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿string[] arr = { "1234", "1567", "-2", "computer science"};
+
+string[] FilterFourChar(string[] array)
+{
+    array = Array.FindAll(array, el => el.Length < 4);
+    return array;
+}
+
+foreach (string item in FilterFourChar(arr))
+{
+    Console.WriteLine(item);
+}
